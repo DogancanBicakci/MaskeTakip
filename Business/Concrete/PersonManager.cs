d8bf38f772240d2e8da05b991dcc0b5f37393750
Doğancan Bıçakçı <dogi1161520@gmail.com>
2024-02-15 21:43:13 +0300

    Add project files.

---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PersonManager
+    {
+        //Encapsulation denir bu metota, gerekli parametreleri bir metot içinde tutuyorum. Yeni bir şey gelirse içine ekle patlamaz...
+        public void ApplyForMask(Person person)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿global using global::System;
+
+namespace Workaround
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            SelamVer(isim:"ali");
+            SelamVer(isim:"doğancan");
+            SelamVer();
+
+            int sonuc = Topla();
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim="isimsiz") //Default parametre deniyor
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla()
+        {
+            return 5;
+        }
+
+       
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
